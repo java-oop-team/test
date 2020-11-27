9e6ad5b3e7f5cc87695530406fdc8fb62bc30c20
kavindu_prabhashitha <donprabhashitha@gmail.com>
2020-11-27 12:50:52 +0530

    Teacher.java class edited

--- testApp/src/testapp/Teacher.java
+++ testApp/src/testapp/Teacher.java
@@ -10,5 +10,45 @@ package testapp;
  * @author MSI-10scxr
  */
 public class Teacher {
+    private String fname;
+    private String lname;
+    private String address;
+
+    public Teacher(String fname, String lname, String address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.address = address;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" + "fname=" + fname + ", lname=" + lname + ", address=" + address + '}';
+    }
+    
+    
     
 }
