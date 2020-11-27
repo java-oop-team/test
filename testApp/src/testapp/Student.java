16afc4e4f1804e2d17515e38e69795dc304ee2b1
lakchani <vivekalakchani@gmail.com>
2020-11-27 13:30:11 +0530

    Student file update

--- testApp/src/testapp/Student.java
+++ testApp/src/testapp/Student.java
@@ -10,5 +10,47 @@ package testapp;
  * @author MSI-10scxr
  */
 public class Student {
-    
+     private String name;
+     private String address;
+     private double phoneNo;
+
+    public Student(String name, String address, double phoneNo) {
+        this.name = name;
+        this.address = address;
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNo(double phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", address=" + address + ", phoneNo=" + phoneNo + '}';
+    }
+     
+     
+     
+     
+     
 }
