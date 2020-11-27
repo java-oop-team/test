5ea5f8432ea5a852394ef61cf17238a2bc56300b
Kavishan Kavinda <kavishankavinda5@gmail.com>
2020-11-27 12:29:24 +0530

    Principle.java class added

---
+++ testApp/src/testapp/Principle.java
@@ -0,0 +1,30 @@
+package testapp;
+
+public class Principle {
+    
+    private String strName;
+    private int intAge;
+
+    public Principle(String strName, int intAge) {
+        this.strName = strName;
+        this.intAge = intAge;
+    }
+
+    public String getStrName() {
+        return strName;
+    }
+
+    public int getIntAge() {
+        return intAge;
+    }
+
+    public void setStrName(String strName) {
+        this.strName = strName;
+    }
+
+    public void setIntAge(int intAge) {
+        this.intAge = intAge;
+    }
+    
+    
+}
